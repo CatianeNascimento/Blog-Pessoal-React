e03147604d158527410674254a7d1e2c8d904257
CatianeNascimento <csnascimento07@gmail.com>
2023-04-11 15:08:06 -0300

    criação pagina home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className='titulo'>Bem-vindes ao Meu Blog Pessoal</h1>
+            <img src="src/assets/memoji.svg" alt="Imagem pagina principal" className='img-home' />
+        </>
+    );
+}
+
+export default Home;
